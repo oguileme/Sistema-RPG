770bea31ab49a330bc1333feb02664d6d201bc2f
LorenzoNardon <lorenzofrigo.aluno@unipampa.edu.br>
2026-09-18 19:38:55 -0300

    Adicionar Ficha

---
+++ classes/Ficha.java
@@ -0,0 +1,26 @@
+package classes;
+
+public abstract class Ficha {
+    private int vidaMax;
+    private int vidaAtual;
+    private int manaMax;
+    private int manaAtual;
+    private String nome;
+    private String classe;
+    private String raca;
+    private String origem;
+    private String divindade;
+    private int pontosExp;
+    private int idade;
+    private Double tamanho;
+    private Double deslocamento;
+    private int dinheiro;
+    private int forca;
+    private int destreza;
+    private int constituicao;
+    private int inteligencia;
+    private int sabedoria;
+    private int carisma;
+
+
+}
