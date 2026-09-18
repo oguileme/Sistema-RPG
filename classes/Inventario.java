7ff23a204c1bfc5ec363c6b1654e140c92ab6794
Eduardo-Antunes-00 <eantunesdasilva00@gmail.com>
2026-09-18 19:18:10 -0300

    Add files via upload

---
+++ classes/Inventario.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Inventario {
+    private List<Equipamento> equipamentos;
+    private int cargaMax;
+    private int cargaAtual;
+
+}
---
+++ classes/Rolagem.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Rolagem {
+    private List<String> tiposDado;
+    private List<Integer> quantidades;
+    private int somaResultado;
+    private int resultadoFinal;
+}
