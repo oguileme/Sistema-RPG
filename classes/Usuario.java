3bac3a6a2f69d3e987a4375e587424ceed3eae2c
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-09-23 21:08:21 -0300

    feat(usuario) adiciona metodo construtor + getters e setters em usuarios

--- classes/Usuario.java
+++ classes/Usuario.java
@@ -4,4 +4,38 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
+
+    //**Método construtor de Usuario */
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    //**Método getters */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    //**Método setters */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
