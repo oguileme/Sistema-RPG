25ed6ad75d183c0bdfe4ecdcd7b65f7cb95c451e
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-09-23 21:25:58 -0300

    feat(campanha) adiciona atributos de players participante e fichas, método construtor, getters, setteer e metodos para adicionar ficha e player nova

--- classes/Campanha.java
+++ classes/Campanha.java
@@ -1,4 +1,48 @@
 public class Campanha {
     private String nome;
     private String descricao;
+    private Mestre mestre;
+    private List<Player> jogadores;
+    private List<Ficha> fichas;
+
+    //**Método construtor de Campanha */
+    public Campanha(String nome, String descricao, Mestre mestre) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.mestre = mestre;
+    }
+
+    //**Método getters */
+    public String getNome() {
+       return nome;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public Mestre getMestre() {
+        return mestre;
+    }
+
+    //**Método setters*/
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //sem setter de mestre, pois o mestre é definido no momento da criação da campanha e não deve ser alterado posteriormente.
+
+    public void addPlayer(Player player) {
+        // lógica para adicionar um jogador à campanha
+        this.jogadores.add(player);
+    }
+
+    public void addFicha(Ficha ficha){
+        this.fichas.add(ficha);
+    }
+
 }
